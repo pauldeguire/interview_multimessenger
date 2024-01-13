e663c6f4a29090afb24ac9f5c485f26185ab1f26
Paul Deguire <pdeguire@Air-de-Paul.gateway.sasktel.net>
2024-01-13 11:06:06 -0600

    Coefficient of variation

--- Exercise/__main__.py
+++ Exercise/__main__.py
@@ -24,22 +24,22 @@ for file in list_files:
     toplot1.append(((x,y,name),'plot'))
     ys.append(y)
     
-fun.plot_zoom([1.5e8,4.5e8],toplot1)
+fun.plot_zoom([1.5e8,4.5e8],toplot1,name_fig='all_amp')
 
 
 
 ##
-## We see the amplifiers seem to obey the same pattern
+## We see the amplifiers seem to obey the same pattern, but some have higher gains than others.
+## Let's asssume all the amplifiers were designed to have the same characteristics and find a mean value and a std
 ##
 
 ys = np.asarray(ys)
 std = ys.std(0)
 mean = ys.mean(0)
 toplot2 = [((x,mean,std),'uncert')]
-fun.plot_zoom([1.5e8,4e8],toplot2)
+fun.plot_zoom([1.5e8,4.5e8],toplot2,name_fig='uncert')
 
 
-plt.plot(x,std/mean)
-#plt.xlim([1.5e8,4.5e8])
-#plt.ylim([0.01,0.02])
-plt.show()
+toplot3 = [((x,std/mean,'$\\sigma/\\mu$'),'plot')]
+fun.plot_zoom([1e8,6e8],toplot3,name_fig='cv',place='up',ylab='Coefficient of variation')
+
--- Exercise/functions.py
+++ Exercise/functions.py
@@ -11,37 +11,40 @@ def find_closer(array,braket):
     return [idx_min,idx_max+1]
 
 
-def plot_zoom(xvals,to_plot,xlab='Frequency (Hz)',ylab='Amplification magnitude'):
+def plot_zoom(xvals,to_plot,place='down',name_fig='test',xlab='Frequency (Hz)',ylab='Amplification magnitude'):
     """
-    Input:
-    Output:
+    Input: A list with a x interval for the subplot inside the big plot, and a list to know what to plot 
+    Output: The figure is saved in the Figures folder and is shown
     """
     fig, ax1 = plt.subplots(figsize=(9,4))
-    l, b, h, w = .25, .3, .3, .4
+    if place=='down':l, b, h, w = .25, .3, .3, .4 # To place the subplot
+    elif place=='up':l, b, h, w = .25, .5, .3, .4 # To place the subplot
     ax2 = fig.add_axes([l, b, w, h])
     for i in to_plot:
         params,typ = i
-        if typ=='plot':
+        if typ=='plot': # Used for the first figure of the exercise
             x,y,label=params
             down,up=find_closer(x,xvals)
             ax1.plot(x,y,label=label)
             ax2.plot(x[down:up],y[down:up])
-        elif typ=='uncert':
+        elif typ=='uncert': # Used for the second figure of the exercise
             x,ymean,yerr = params
             down,up=find_closer(x,xvals)
             ax1.plot(x,ymean,label='Mean value')
             ax2.plot(x[down:up],ymean[down:up])
             ax1.fill_between(x,ymean-yerr,ymean+yerr,color='blue',alpha=0.2,label='Standard deviation (1$\\sigma$)')
             ax2.fill_between(x[down:up],ymean[down:up]-yerr[down:up],ymean[down:up]+yerr[down:up],color='blue',alpha=0.2)
+        else: raise ValueError('You need to chose between \'plot\' and \'uncert\' for the type')
     ax1.set_xlabel(xlab, fontsize = 15)
     ax1.set_ylabel(ylab, fontsize=15)
     ax1.legend()
     fig.tight_layout()
+    plt.savefig('Exercise/Figures/'+name_fig+'.png')
     plt.show()
 
 
 if __name__=='__main__':
     x = np.arange(0,2*np.pi,0.01)
     y = -np.cos(x)
-    plot_zoom([1,2],[((x,y,'test'),'plot')],xlab='x',ylab='y')
+    plot_zoom([1,2],[((x,y,'test'),'plot')],xlab='x',ylab='y',place='up')
     
