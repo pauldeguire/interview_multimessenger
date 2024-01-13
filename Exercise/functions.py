f45bb2b1fef02dc9c7733cf7566b1cc17cdd8e49
Paul Deguire <pdeguire@Air-de-Paul.gateway.sasktel.net>
2024-01-13 10:25:46 -0600

    Add if __name__=='__main__'

--- Exercise/functions.py
+++ Exercise/functions.py
@@ -11,7 +11,7 @@ def find_closer(array,braket):
     return [idx_min,idx_max+1]
 
 
-def plot_zoom(xvals,to_plot):
+def plot_zoom(xvals,to_plot,xlab='Frequency (Hz)',ylab='Amplification magnitude'):
     """
     Input:
     Output:
@@ -19,14 +19,11 @@ def plot_zoom(xvals,to_plot):
     fig, ax1 = plt.subplots(figsize=(9,4))
     l, b, h, w = .25, .3, .3, .4
     ax2 = fig.add_axes([l, b, w, h])
-    diffs=[]
-    print(to_plot)
     for i in to_plot:
         params,typ = i
         if typ=='plot':
             x,y,label=params
             down,up=find_closer(x,xvals)
-            print(label)
             ax1.plot(x,y,label=label)
             ax2.plot(x[down:up],y[down:up])
         elif typ=='uncert':
@@ -36,13 +33,15 @@ def plot_zoom(xvals,to_plot):
             ax2.plot(x[down:up],ymean[down:up])
             ax1.fill_between(x,ymean-yerr,ymean+yerr,color='blue',alpha=0.2,label='Standard deviation (1$\\sigma$)')
             ax2.fill_between(x[down:up],ymean[down:up]-yerr[down:up],ymean[down:up]+yerr[down:up],color='blue',alpha=0.2)
-    ax1.set_xlabel('Frequency (Hz)', fontsize = 15)
-    ax1.set_ylabel('Amplification magnitude', fontsize=15)
+    ax1.set_xlabel(xlab, fontsize = 15)
+    ax1.set_ylabel(ylab, fontsize=15)
     ax1.legend()
     fig.tight_layout()
     plt.show()
-    #ax1.set_yscale("log")
-    #ax1.set_xlabel('$n$', fontsize = 15)
-    #ax1.set_ylabel('$\log(|A_n(0.1) - Z(0.1)|)$', fontsize=15)
-    #ax1.set_title(r'$g=0.1$', fontsize=15)
+
+
+if __name__=='__main__':
+    x = np.arange(0,2*np.pi,0.01)
+    y = -np.cos(x)
+    plot_zoom([1,2],[((x,y,'test'),'plot')],xlab='x',ylab='y')
     
